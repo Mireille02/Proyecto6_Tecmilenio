e5cbe095fd61db2c36856051872a17d4beefd9f5
Mireille <mire.flor4@gmail.com>
2023-05-30 23:59:51 -0600

    final

--- final/src/database.js
+++ final/src/database.js
@@ -7,3 +7,16 @@ mongoose.connect('mongodb://localhost/notes-db-app', {
 })
   .then(db => console.log('DB is connected'))
   .catch(err => console.error(err));
+
+/*const URI = process.env.notes-db-app;
+
+mongoose.connect(URI, {
+
+useNewUrlParser: true, 
+
+useUnifiedTopology: true 
+
+}, err => {
+if(err) throw err;
+console.log('Connected to MongoDB!!!')
+});*/
